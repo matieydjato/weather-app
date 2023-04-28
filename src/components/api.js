7127d5070ef0329e3b29ba2dfddd4bedc597613a
Matieyendou DJATO <MDJATO@digijob.local>
2023-04-28 15:18:22 +0000

    React JS Weahther App

---
+++ src/components/api.js
@@ -0,0 +1,12 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "7a4a422ac0459929cb3109a034ef50d4";
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'content-type': 'application/octet-stream',
+		'X-RapidAPI-Key': '88ec61133cmsh98907097f206f9bp10cd58jsn657f41b5cffc',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
